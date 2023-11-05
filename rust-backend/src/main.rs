0d14e7872a6381458af9212d6c6638ca332bc138
Chris Tague <cstague6401@gmail.com>
2023-11-05 11:03:23 -0600

    work in progress adding tailwind to the htmx in the rust backend

--- rust-backend/src/main.rs
+++ rust-backend/src/main.rs
@@ -1,12 +1,18 @@
 #[macro_use] extern crate rocket;
 
-use rocket::response::content::RawHtml;
+use rocket::http::ContentType;
+use rocket::response::{content::RawHtml, Response};
+use rocket::State;
+use std::{io, fs::{self, File}};
+
+struct CssFilePath(String);
 
 #[get("/")]
 fn index() -> RawHtml<&'static str> {
     RawHtml(r#"
+<link rel="stylesheet" href="output.css">
 <script src="https://unpkg.com/htmx.org@1.9.7"></script>
-<button hx-post="/clicked" hx-swap="outerHTML">
+<button class="p-6" hx-post="/clicked" hx-swap="outerHTML">
     Click Me
 </button>
     "#)
@@ -19,7 +25,19 @@ fn clicked() -> RawHtml<&'static str> {
     "#)
 }
 
+#[get("/output.css")]
+fn css(file_path: &State<CssFilePath>) -> String {
+    let contents = fs::read_to_string(&file_path.0)?;
+    Response::build()
+        .header(ContentType::CSS)
+        .sized_body(contents.len(), io::Cursor::new(contents))
+        .finalize()
+    
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, clicked])
+    rocket::build()
+        .mount("/", routes![index, clicked])
+        .manage(CssFilePath("./dist/output.css".to_string()))
 }
